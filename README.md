cf21d855f138fa75da67408286826910224bcf46
nadeemakhter0602 <65607791+nadeemakhter0602@users.noreply.github.com>
2022-12-27 21:39:38 +0530

    implement announce request

--- README.md
+++ README.md
@@ -3,4 +3,5 @@
 Implementation of certain parts of the BitTorrent Protocol Specification v1.
 
 * ~~Implement [BEncode](https://en.wikipedia.org/wiki/Bencode) parsing, encoding and decoding of .torrent files and BEncoded data with unit tests.~~ DONE.
-* Implement [Announce request](https://wiki.vuze.com/w/Announce) to [Trackers](https://en.wikipedia.org/wiki/BitTorrent_tracker) and retrieving [Peer](https://en.wikipedia.org/wiki/Glossary_of_BitTorrent_terms#Peer) List. In Progress.
+* ~~Implement [Announce request](https://wiki.vuze.com/w/Announce) to [Trackers](https://en.wikipedia.org/wiki/BitTorrent_tracker) and retrieving [Peer](https://en.wikipedia.org/wiki/Glossary_of_BitTorrent_terms#Peer) List.~~ DONE.
+* Implement Peer Connection. In Progress.
---
+++ announce.py
@@ -0,0 +1,51 @@
+from bencoding import BEncoding
+import hashlib
+import requests
+import secrets
+
+
+def announce(torrent_file, peer_id, port, uploaded, downloaded, left, event, compact):
+    trackers = torrent_file['announce']
+    info_hash = bencoding.encode(torrent_file['info'])
+    hashing = hashlib.new('sha1')
+    hashing.update(info_hash)
+    digest = hashing.digest()
+    query_params = {
+        'info_hash': digest,
+        'peer_id': peer_id,
+        'port': port,
+        'uploaded': uploaded,
+        'downloaded': downloaded,
+        'left': left,
+        'event': event,
+        'compact': compact
+    }
+    try:
+        response = requests.get(trackers, params=query_params)
+        return bencoding.decode(response.content)
+    except Exception as e:
+        print(str(e))
+
+
+peer_id = bytes.fromhex(secrets.token_hex(20))
+bencoding = BEncoding()
+torrent_file = bencoding.decode_file('test.torrent')
+length = str(torrent_file['info']['length'])
+event = 'started'
+port = '6881'
+compact = '1'
+uploaded = 0
+downloaded = 0
+
+tracker_response = announce(torrent_file, peer_id,
+                            port, uploaded, downloaded, length, event, compact)
+peer_bytes = tracker_response['peers']
+peer_list = []
+for i in range(0, len(peer_bytes), 6):
+    start = i
+    end = i + 6
+    peer = peer_bytes[start:end]
+    first_4 = [byte for byte in peer[:4]]
+    last_2 = int.from_bytes(peer[4::], "big")
+    peer_list.append((first_4, last_2))
+print(peer_list)
