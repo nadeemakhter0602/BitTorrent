99d9179e1d412aaafc1099b18c3607c039275e9e
nadeemakhter0602 <65607791+nadeemakhter0602@users.noreply.github.com>
2023-01-31 20:43:40 +0530

    modularize code

--- announce.py
+++ announce.py
@@ -26,26 +26,26 @@ def announce(torrent_file, peer_id, port, uploaded, downloaded, left, event, com
     except Exception as e:
         print(str(e))
 
+if __name__ == '__main__':
+    peer_id = bytes.fromhex(secrets.token_hex(20))
+    bencoding = BEncoding()
+    torrent_file = bencoding.decode_file('test.torrent')
+    length = str(torrent_file['info']['length'])
+    event = 'started'
+    port = '6881'
+    compact = '1'
+    uploaded = 0
+    downloaded = 0
 
-peer_id = bytes.fromhex(secrets.token_hex(20))
-bencoding = BEncoding()
-torrent_file = bencoding.decode_file('test.torrent')
-length = str(torrent_file['info']['length'])
-event = 'started'
-port = '6881'
-compact = '1'
-uploaded = 0
-downloaded = 0
-
-tracker_response = announce(torrent_file, peer_id,
-                            port, uploaded, downloaded, length, event, compact)
-peer_bytes = tracker_response['peers']
-peer_list = []
-for i in range(0, len(peer_bytes), 6):
-    start = i
-    end = i + 6
-    peer = peer_bytes[start:end]
-    first_4 = [byte for byte in peer[:4]]
-    last_2 = int.from_bytes(peer[4::], "big")
-    peer_list.append((first_4, last_2))
-print(peer_list)
+    tracker_response = announce(torrent_file, peer_id,
+                                port, uploaded, downloaded, length, event, compact)
+    peer_bytes = tracker_response['peers']
+    peer_list = []
+    for i in range(0, len(peer_bytes), 6):
+        start = i
+        end = i + 6
+        peer = peer_bytes[start:end]
+        first_4 = [byte for byte in peer[:4]]
+        last_2 = int.from_bytes(peer[4::], "big")
+        peer_list.append((first_4, last_2))
+    print(peer_list)
