f6742a28f8e0301a26f6bbc3002139dcd6a590d1
nadeemakhter0602 <65607791+nadeemakhter0602@users.noreply.github.com>
2023-04-01 18:53:05 +0530

    change Peer class and use it for peer connection

--- peer.py
+++ peer.py
@@ -1,6 +1,10 @@
 import secrets
 
 class Peer:
-    def __init__(self, port):
-        self.peer_id = bytes.fromhex(secrets.token_hex(20))
+    def __init__(self, port, ipaddr=None, peer_id=None):
+        if not peer_id:
+            self.peer_id = bytes.fromhex(secrets.token_hex(20))
+        else:
+            self.peer_id = peer_id
         self.port = port
+        self.ipaddr = ipaddr
--- peer_connection.py
+++ peer_connection.py
@@ -6,10 +6,13 @@ import traceback
 
 
 class Connection:
-    def __init__(self, peer, torrent):
+    def __init__(self, client, peer, torrent):
+        self.client = client
         self.peer = peer
+        self.ip_port = (self.peer.ipaddr, self.peer.port)
         self.torrent = torrent
         self.conn = socket.socket()
+        self.is_connected = False
         self.protocol_identifier = b'\x13'
         self.pstr = b'BitTorrent protocol'
         self.reserved_bytes = b'\x00\x00\x00\x00\x00\x00\x00\x00'
@@ -23,18 +26,22 @@ class Connection:
         self.msg_piece = 7
         self.msg_cancel = 8
 
-    def connect_peer(self, peer):
+    def connect_peer(self):
         try:
-            print("Attempting to connect to %s" % str(peer))
-            self.conn.connect(peer)
-            print("Successfully connected to %s" % str(peer))
+            print("Attempting to connect to %s" % str(self.ip_port))
+            self.conn.connect(self.ip_port)
+            self.is_connected = True
+            print("Successfully connected to %s" % str(self.ip_port))
         except Exception as err:
             print("Connection failed due to error :")
             traceback.print_exc()
             pass
 
-    def send_handshake(self, peer):
+    def send_handshake(self):
         try:
+            if not self.is_connected:
+                print("No peer connected")
+                return
             handshake = self.serialize_handshake()
             print("Sending handshake :", handshake)
             self.conn.send(handshake)
@@ -43,10 +50,11 @@ class Connection:
             deserialized = self.deserialize_handshake(recv)
             print("Deserialized response :", deserialized)
             protocol_identifier, pstr, reserved_bytes, info_hash, peer_id = deserialized
+            self.peer.peer_id = peer_id
             if info_hash == self.torrent.info_hash:
                 print("Handshake Completed, Peer ID is %s" % peer_id)
             else:
-                print("Invalid Info Hash received from Peer %s with ID %s" % (str(peer), peer_id))
+                print("Invalid Info Hash received from Peer %s with ID %s" % (str(self.ip_port), peer_id))
         except Exception as err:
             print("Handshake failed due to error :")
             traceback.print_exc()
@@ -60,7 +68,7 @@ class Connection:
         handshake += self.pstr
         handshake += self.reserved_bytes
         handshake += self.torrent.info_hash
-        handshake += self.peer.peer_id
+        handshake += self.client.peer_id
         return handshake
 
     def deserialize_handshake(self, handshake):
@@ -73,7 +81,7 @@ class Connection:
 
 
 if __name__ == '__main__':
-    my_peer = Peer('6881')
+    client = Peer('6881')
     torrent = Torrent('test.torrent', './')
     length = torrent.length
     event = 'started'
@@ -81,7 +89,7 @@ if __name__ == '__main__':
     uploaded = 0
     downloaded = 0
     tracker_response = announce.announce(torrent,
-                                         my_peer,
+                                         client,
                                          uploaded,
                                          downloaded,
                                          length,
@@ -89,10 +97,11 @@ if __name__ == '__main__':
                                          compact)
     peer_list = announce.get_peers(tracker_response)
     for peer in peer_list:
+        peer = Peer(ipaddr=peer[0], port=peer[1])
         print()
-        connection = Connection(my_peer, torrent)
-        connection.connect_peer(peer)
+        connection = Connection(client, peer, torrent)
+        connection.connect_peer()
         print()
-        connection.send_handshake(peer)
+        connection.send_handshake()
         connection.close_connection()
         print()
